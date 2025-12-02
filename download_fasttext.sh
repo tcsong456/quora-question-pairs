6de593f6e8ee1b9be12ea7610e8d4c0a3ecee628
Andy Song <congxisong@hotmail.com>
2025-12-02 20:58:07 +0000

    char embedding done

--- download_fasttext.sh
+++ download_fasttext.sh
@@ -1,5 +1,5 @@
-mkdir -p fasttext_emb
-cd fasttext_emb
+mkdir -p artifacts
+cd artifacts
 echo 'Downloading crawl-300-2M.bin fasttext embedding'
 wget -c https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.en.300.bin.gz
 echo 'Unzipping'
